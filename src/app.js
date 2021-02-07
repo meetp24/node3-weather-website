99525bf294568777d2e608bf514a04c8c2805850
meetp24 <meet.patel1@bacancy.com>
2021-02-07 23:07:56 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templetes/views')
+const partialsPath = path.join(__dirname, '../templetes/partials')
+
+//set handlebar engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Help for Weather App',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/weather',(req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    geocode(req.query.address, (error, { latitute, longitute, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitute, longitute, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
+}) 
+
+app.get('/products',(req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*',(req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help Article Not Found.'
+    })
+})
+
+app.get('*',(req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page Not Found.'
+    })
+}) 
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+}) 
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitute,longitute, callback) => {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitute + '&lon=' + longitute + '&appid=8a57c52bfbb2d9919efe4bd273c23f70'
+    
+    request({ url, json: true }, (error, { body }) => {
+            if (error) {
+                callback('Unable to Connect to Weather-app', undefined)
+            } else if (body.error) {
+                callback('Unable to find Location', undefined)
+            } else {
+                callback(undefined, 'It is currently ' + body.main.temp + ' degrees out. There is a ' + body.main.humidity + '% chance of rain.')
+            } 
+    })
+}
+
+module.exports = forecast 
